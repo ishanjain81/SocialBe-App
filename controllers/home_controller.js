9de8321b892451f165d33e72118adbc3aec89672
Ishan <ishanjaincool81@gmail.com>
2021-02-16 14:37:35 +0530

    Coverting Code to Async Await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,7 +10,7 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
 
     // Post.find({},function(err,posts){
     //     return res.render('home',{
@@ -19,27 +19,48 @@ module.exports.home = function(req,res){
     //     });
     // })
 
-    //popultate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err,posts){
+    // .exec(function(err,posts){
 
-        User.find({}, function(err,users){
-            return res.render('home',{
-                title: "Codeial | Home",
-                posts: posts,
-                all_users: users
-            });
+    //     User.find({}, function(err,users){
+    //         return res.render('home',{
+    //             title: "Codeial | Home",
+    //             posts: posts,
+    //             all_users: users
+    //         });
+    //     });
+
+    // });
+    try{
+         //popultate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+        let users = await User.find({});
+
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users
         });
 
-    });
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
 
 }
 
 // module.exports.actionName = function(req, res){}
+
+
+//using then
+//Post.find({}).populate('comments').then(function());
+
+//let posts = Post.find({}).populate('comments').exec();
+//posts.then();
+
