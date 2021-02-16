50acde6d98be2d38f56f20315965dfc5ecd2bcbd
Ishan <ishanjaincool81@gmail.com>
2021-02-16 16:44:09 +0530

    Setting Up Notifications using Noty.js

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -14,10 +14,11 @@ module.exports.create = async function(req,res){
 
             post.comments.push(comment);
             post.save();
+            req.flash('success','Comment Added!');
             return res.redirect('/');
         }
     }catch(err){
-        console.log('Error',err);
+        req.flash('error',err);
         return;
     }
 }
@@ -31,12 +32,14 @@ module.exports.destroy =  async function(req,res){
             comment.remove();
 
             let post = await Post.findByIdAndUpdate(postId,{ $pull:{comments: req.params.id}});
+            req.flash('success','Comment Deleted!');
             return res.redirect('back');
         }else{
+            req.flash('error','You cannot delete this Comment!');
             return res.redirect('back');
         }
     }catch(err){
-        console.log('Error',err);
+        req.flash('error',err);
         return;
     }
 }
