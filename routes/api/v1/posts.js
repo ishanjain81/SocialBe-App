2b94f0e70453fdffd995f90343978b4fb235ae07
Ishan <ishanjaincool81@gmail.com>
2021-02-20 16:51:40 +0530

    Sign-In/Sign-Up using Google

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('../../../config/passport-jwt-strategy');
+const passport = require('passport');
 
 const postsApi = require("../../../controllers/api/v1/posts_api");
 
