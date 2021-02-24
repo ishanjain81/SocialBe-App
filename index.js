d995d3d90dfb75741992996c57e3ad6b4684ca37
Ishan <ishanjaincool81@gmail.com>
2021-02-24 10:59:05 +0530

    Adding Styles for chat box And add favicon

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
+const path = require('path')
+const favicon = require('serve-favicon');
 
-
-
+app.use(favicon(path.join(__dirname,'/assets/images/favi.ico'))); 
 app.use(sassMiddleware({
     src: './assets/scss',
     dest: './assets/css',
