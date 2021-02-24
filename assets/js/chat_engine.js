c8e799b44bb57a1387f1283d753883d6104a627f
Ishan <ishanjaincool81@gmail.com>
2021-02-24 14:48:41 +0530

    Sending And Recieving Messages

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,7 +1,8 @@
 class ChatEngine{
-    constructor(chatBoxId,userEmail){
+    constructor(chatBoxId,userEmail,userName){
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
+        this.userName = userName;
 
         this.socket = io.connect('http://localhost:5000');
 
@@ -11,8 +12,60 @@ class ChatEngine{
     }
 
     connectionHandler(){
+        let self = this;
+
         this.socket.on('connect',function(){
             console.log('connection established using sockets....!');
+
+            self.socket.emit('join_room',{
+                user_email: self.userEmail,
+                user_name: self.userName,
+                chatroom: 'codeial'
+            });
+
+            self.socket.on('user_joined',function(data){
+                console.log('a user joined',data);
+            });
+
+            $('#send-message').click(function(){
+                let msg = $('#chat-message-input').val();
+
+                if(msg != ''){
+                    self.socket.emit('send_message',{
+                        message: msg,
+                        user_email: self.userEmail,
+                        user_name: self.userName,
+                        chatroom: 'codeial'
+                    });
+                }
+            });
+
+            self.socket.on('recieve_message',function(data){
+                console.log('message recieved',data.message);
+
+                let newMessage = $('<li>');
+
+                let messageType = 'other-message';
+
+                if(data.user_email == self.userEmail){
+                    messageType = 'self-message';
+                }
+
+                newMessage.append($('<span>',{
+                    'html': data.message
+                }));
+
+                newMessage.append($('<br>'));
+
+                newMessage.append($('<sub>',{
+                    'html' : data.user_name
+                }));
+
+                newMessage.addClass(messageType);
+
+                $('#chat-messages-list').append(newMessage);
+            });
+
         });
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -14,6 +14,20 @@ module.exports.chatSockets = function(socketServer){
          socket.on('disconnect',function(){
             console.log('socket disconnected!')
          });
+
+         socket.on('join_room',function(data){
+            console.log('joining request rec..',data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined',data);
+         });
+
+
+         socket.on('send_message',function(data){
+            io.in(data.chatroom).emit('recieve_message',data);
+         });
+
     });
 
 }
