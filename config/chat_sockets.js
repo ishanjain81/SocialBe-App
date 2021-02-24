b0c64cf0bcf9b5a39104862ff8280f0364bedaec
Ishan <ishanjaincool81@gmail.com>
2021-02-24 13:24:43 +0530

    Establishing connection using sockets

---
+++ config/chat_sockets.js
@@ -0,0 +1,19 @@
+
+
+module.exports.chatSockets = function(socketServer){
+    let io = require('socket.io')(socketServer,{
+        cors: {
+            origin: "http://localhost:8000",
+            credentials: true
+        }
+    });
+    
+    io.sockets.on('connection',function(socket){
+         console.log('new connection received',socket.id);
+
+         socket.on('disconnect',function(){
+            console.log('socket disconnected!')
+         });
+    });
+
+}
