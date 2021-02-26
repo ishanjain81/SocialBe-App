e41349f3134eb83017fc3ed5b69032b45578f222
Ishan <ishanjaincool81@gmail.com>
2021-02-26 12:38:42 +0530

    Setting Up Production Environment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
-const path = require('path')
+const path = require('path');
 const favicon = require('serve-favicon');
 
 app.use(favicon(path.join(__dirname,'/assets/images/favi.ico'))); 
