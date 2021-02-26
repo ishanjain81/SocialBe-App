7fabee6c104e5d2c4a8451aecd3e24cada428c42
Ishan <ishanjaincool81@gmail.com>
2021-02-26 11:36:58 +0530

    Setting Up Development Environment

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -2,12 +2,13 @@ const passport = require('passport');
 const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const crypto = require('crypto');
 const User = require('../models/user');
+const env = require('./environment');
 
 //tell passport to use a new strategy for google login
 passport.use(new googleStrategy({
-        clientID: "181517010512-mc0e2ni0i202jucnanplr1p300a109cm.apps.googleusercontent.com",
-        clientSecret: "yRvgtfF1wY7yCvoYVLVjQkRA",
-        callbackURL: "http://localhost:8000/users/auth/google/callback",
+        clientID: env.google_client_id,
+        clientSecret: env.google_client_secret,
+        callbackURL: env.google_call_back_url,
     },
     function(accessToken,refreshToken,profile,done){
         // find a user 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const env = require('./config/environment');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
@@ -26,8 +27,8 @@ chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
 
 app.use(sassMiddleware({
-    src: './assets/scss',
-    dest: './assets/css',
+    src: path.join(__dirname,env.asset_path, './scss'),
+    dest: path.join(__dirname,env.asset_path, './css'),
     debug: true,
     outputStyle: 'extended',
     prefix: '/css'
@@ -37,7 +38,7 @@ app.use(sassMiddleware({
 app.use(express.urlencoded());
 app.use(cookieParser());
 
-app.use(express.static('./assets'));
+app.use(express.static(env.asset_path));
 //make the uploads path available to the browser
 app.use('/uploads',express.static(__dirname + '/uploads'));
 
@@ -55,7 +56,7 @@ app.set('views','./views');
 app.use(session({
     name: 'codeial',
     //TODO change the secret before deployment in production mode
-    secret: 'blahsomething',
+    secret: env.session_cookie_key,
     saveUninitialized: false,
     resave: false,
     cookie: {
